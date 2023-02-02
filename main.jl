98ecb9161ae6547b9f5956482d51ab66f187c8a9
Daryll Ko <daryll.ko@gmail.com>
2023-02-02 13:47:38 +0800

    fix: tabs —> spaces

--- main.jl
+++ main.jl
@@ -1,15 +1,15 @@
 import JSON
 
 function main()
-	open("reviews.json", "r") do f
-		s = read(f, String)
-		j = JSON.parse(s)
-		jp_en_cards = j["cards_vocabulary_jp_en"]
-		for i in eachindex(jp_en_cards)
-			display(jp_en_cards[i]["reviews"])
-			break
-		end
-	end
+    open("reviews.json", "r") do f
+        s = read(f, String)
+        j = JSON.parse(s)
+        jp_en_cards = j["cards_vocabulary_jp_en"]
+        for i in eachindex(jp_en_cards)
+            display(jp_en_cards[i]["reviews"])
+            break
+        end
+    end
 end
 
 main()
