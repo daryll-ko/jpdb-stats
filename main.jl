a912c7ac50301b92c3655b193ed26bda5e21ba21
Daryll Ko <daryll.ko@gmail.com>
2023-02-02 21:25:06 +0800

    refactor: use `get_review_frequencies` as a name instead

--- main.jl
+++ main.jl
@@ -6,14 +6,14 @@ include("./Cards.jl")
 
 import .Cards: Card, Review, parse_cards
 
-function get_word_review_frequency_table(cards)
+function get_review_frequencies(cards)
     rows = [(word=card.spelling, review_frequency=length(card.reviews)) for card in cards]
     return DataFrame(rows)
 end
 
 function main()
     open("reviews.json", "r") do f
-        read(f, String) |> JSON.parse |> (j -> j["cards_vocabulary_jp_en"]) |> parse_cards |> get_word_review_frequency_table |> display
+        read(f, String) |> JSON.parse |> (j -> j["cards_vocabulary_jp_en"]) |> parse_cards |> get_review_frequencies |> display
     end
 end
 
