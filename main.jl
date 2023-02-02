626dda6b9addcaccf9d8f4ff859b205c0e721698
Daryll Ko <daryll.ko@gmail.com>
2023-02-02 21:11:25 +0800

    feat: add review frequency table

--- main.jl
+++ main.jl
@@ -1,8 +1,15 @@
 import JSON
 
+using DataFrames
+
 include("./Cards.jl")
 
 import .Cards: Card, Review, parse_cards
+
+function get_word_review_frequency_table(cards)
+    rows = [(word=card.spelling, review_frequency=length(card.reviews)) for card in cards]
+    return DataFrame(rows)
+end
 
 function main()
     open("reviews.json", "r") do f
@@ -10,10 +17,7 @@ function main()
         j = JSON.parse(s)
         jp_en_deck = j["cards_vocabulary_jp_en"]
         jp_en_cards = parse_cards(jp_en_deck)
-        for i in eachindex(jp_en_cards)
-            display(jp_en_cards[i])
-            break
-        end
+        display(get_word_review_frequency_table(jp_en_cards))
     end
 end
 
