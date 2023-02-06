67950ef9fc15407fea60e10e663bef211034bef6
Daryll Ko <daryll.ko@gmail.com>
2023-02-06 20:03:59 +0800

    feat: add latest review to table

--- main.jl
+++ main.jl
@@ -1,3 +1,4 @@
+import Dates
 import JSON
 import DataFrames as DF
 
@@ -5,21 +6,34 @@ include("./Cards.jl")
 
 import .Cards: Card, Review, parse_cards
 
-function get_review_frequencies(cards)
+function convert_timestamp(timestamp)
+    return Dates.format(Dates.unix2datetime(timestamp), "U d, yyyy")
+end
+
+function get_latest_timestamp(reviews)
+    latest = 0
+    for review in reviews
+        latest = max(latest, review.timestamp)
+    end
+    return latest
+end
+
+function tabulate_data(cards)
     rows = [
         (
             word=card.spelling,
             review_frequency=length(card.reviews),
+            latest_review=(convert_timestamp ∘ get_latest_timestamp)(card.reviews)
         ) for card in cards
     ]
     df = DF.DataFrame(rows)
-    sort!(df, [:review_frequency], rev=[true])
     return df
 end
 
 function main()
     open("reviews.json", "r") do f
-        read(f, String) |> JSON.parse |> (j -> j["cards_vocabulary_jp_en"]) |> parse_cards |> get_review_frequencies |> display
+        df = read(f, String) |> JSON.parse |> (j -> j["cards_vocabulary_jp_en"]) |> parse_cards |> tabulate_data
+        display(df[1:10, :])
     end
 end
 
