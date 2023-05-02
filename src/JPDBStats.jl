558874c8586a16922985830bef1506155a333df5
Daryll Ko <daryll.ko@gmail.com>
2023-05-02 10:37:41 +0800

    feat: add `group_results_by_date` and `plot_reviews_by_result`

--- src/JPDBStats.jl
+++ src/JPDBStats.jl
@@ -4,7 +4,7 @@ using CSV
 using DataFrames
 using Dates
 using JSON
-using PlotlyJS
+using Plots
 
 #= Structs =#
 
@@ -60,6 +60,37 @@ end
 
 #= Groupings =#
 
+function is_new(result)
+    return result == "unknown"
+end
+
+function is_passing(result)
+    return result == "okay" || result == "pass"
+end
+
+function is_failing(result)
+    return result == "nothing" || result == "fail"
+end
+
+function group_results_by_date(reviews)
+    new_counter = Dict{Date,Int}()
+    pass_counter = Dict{Date,Int}()
+    fail_counter = Dict{Date,Int}()
+
+    for review in reviews
+        date = Date(review.datetime)
+        if is_new(review.grade)
+            new_counter[date] = get(new_counter, date, 0) + 1
+        elseif is_passing(review.grade)
+            pass_counter[date] = get(pass_counter, date, 0) + 1
+        elseif is_failing(review.grade)
+            fail_counter[date] = get(fail_counter, date, 0) + 1
+        end
+    end
+
+    return new_counter, pass_counter, fail_counter
+end
+
 function group_by_date(reviews)
     counter = Dict{Date,Int}()
     for review in reviews
@@ -112,9 +143,21 @@ end
 
 #= Views =#
 
-function plot_counter(counter)
-    data = bar(; x=collect(keys(counter)), y=collect(values(counter)))
-    plot(data)
+function plot_reviews_by_result(reviews)
+    plotlyjs()
+
+    new_counter, pass_counter, fail_counter = group_results_by_date(reviews)
+
+    new_data = bar(collect(keys(new_counter)), collect(values(new_counter)), name="New", marker_color="rgb(99,110,250)", linewidth=0)
+
+    pass_data = bar(x=collect(keys(pass_counter)), y=collect(values(pass_counter)), name="Pass", marker_color="rgb(0,204,150)", linewidth=0)
+
+    fail_data = bar(x=collect(keys(fail_counter)), y=collect(values(fail_counter)), name="Fail", marker_color="rgb(210,77,53)", linewidth=0)
+
+    data = [fail_data, pass_data, new_data]
+    layout = Layout(plot_bgcolor="rgb(17,17,17)", paper_bgcolor="rgb(17,17,17)", barmode="stack")
+
+    plot(data, layout)
 end
 
 function tabulate_card_data(cards)
